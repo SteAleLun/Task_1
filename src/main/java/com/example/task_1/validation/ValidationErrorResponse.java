48151f71f726133bf002567778cbc29be2ced678
SteAleLun <stealelun@gmail.com>
2022-10-27 06:47:47 +0400

    commit

---
+++ src/main/java/com/example/task_1/validation/ValidationErrorResponse.java
@@ -0,0 +1,13 @@
+package com.example.task_1.validation;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class ValidationErrorResponse {
+    private final List<Violation> violations;
+}
+
